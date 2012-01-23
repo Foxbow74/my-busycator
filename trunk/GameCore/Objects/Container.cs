5bef8dbd66e5293759ad8020b9ee994fbffa7ca8
d.chernishenko@gmail.com <d.chernishenko@gmail.com@fb6106e4-4e28-d9a6-f8f4-817c514271ea>
2012-01-23 06:49:58 +0000

    fix project

---
+++ trunk/GameCore/Objects/Container.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Graphics;
+
+namespace GameCore.Objects
+{
+	public abstract class Container:Object
+	{
+	}
+
+	public class Chest:Container
+	{
+		public override Tile Tile
+		{
+			get { return Tiles.ChestTile; }
+		}
+
+		public override string Name
+		{
+			get { return "сундук"; }
+		}
+	}
+}
---
+++ trunk/GameCore/Objects/Door.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Graphics;
+
+namespace GameCore.Objects
+{
+	public class Door:Object
+	{
+		public override Tile Tile
+		{
+			get { return Tiles.DoorTile; }
+		}
+
+		public override string Name
+		{
+			get { return "дверь"; }
+		}
+	}
+}
